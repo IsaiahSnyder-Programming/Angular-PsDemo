3097b21e2f7cf4070361bc5415ff86538e3e023c
isaiahsnyder-programming <isaiahsnyder-programming@gmail.com>
2022-06-21 11:41:42 -0600

    Simple HTTP Client Posting

--- src/app/data/data.service.ts
+++ src/app/data/data.service.ts
@@ -10,6 +10,10 @@ export class DataService {
 
   constructor(private http: HttpClient) { }
 
+  getSubscriptionTypes(): Observable<string[]> {
+    return of(['Monthly', 'Annual', 'Lifetime'])
+  }
+
   postUserSettingsForm(userSettings: UserSettings) : Observable<any> {
 
     return this.http.post('https://putsreq.com/lgsHPeSJEwIoDrlp9LJ4', userSettings)
--- src/app/user-settings-form/user-settings-form.component.ts
+++ src/app/user-settings-form/user-settings-form.component.ts
@@ -1,5 +1,6 @@
 import { Component, NgModule, OnInit } from '@angular/core';
 import { NgForm, NgModel } from '@angular/forms';
+import { Observable } from 'rxjs';
 import { DataService } from '../data/data.service';
 import { UserSettings } from '../data/user-settings';
 
@@ -21,15 +22,16 @@ export class UserSettingsFormComponent implements OnInit {
   userSettings : UserSettings = { ...this.originalUserSettings }
   postError = false;
   postErrorMessage = '';
+  subscriptionTypes: Observable<string[]>;
 
   constructor(private dataService: DataService) { }
 
-  ngOnInit(): void {
+  ngOnInit() {
+    this.subscriptionTypes = this.dataService.getSubscriptionTypes();
   }
 
   onBlur(field: NgModel) {
     console.log('in onBlur', field.valid);
-
   }
 
   onHttpError(errorResponse: any) {
